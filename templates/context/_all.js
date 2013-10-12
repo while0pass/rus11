7f6bde7b2e2d88fd1bd71dea2aab32dbfeff90c5
Andrei Khitrov <a.n.khitrov@gmail.com>
2013-10-12 10:04:18 +0400

    Добавление версии на основе даты в каждый nunjucks-контекст

--- templates/context/_all.js
+++ templates/context/_all.js
@@ -1,5 +1,6 @@
 var now = new Date(),
-    version = require('util').format('v%s.%s.%s.%s', now.getMonth(),
+    version = require('util').format('v%s.%s.%s.%s',
+        ('0' + (now.getMonth() + 1)).slice(-2),
         now.getDate(), now.getHours(), now.getMinutes());
 
 module.exports = {
